1950a0674198f6f10e857f5c78cb880340e02d53
HaNa <postmaster@centos.club>
2026-09-12 15:40:30 +0800

    chore: pip 改用清华镜像安装依赖，提升国内网络下的成功率

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-echo [1/3] 安装打包工具与依赖...
-python -m pip install --upgrade pyinstaller -r requirements.txt || goto :fail
+echo [1/3] 安装打包工具与依赖（清华镜像）...
+python -m pip install --upgrade pyinstaller -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple || goto :fail
 echo [2/3] 打包 Switchboard...
 python -m PyInstaller --noconfirm --clean switchboard.spec || goto :fail
 if not exist "dist\Switchboard\Switchboard.exe" goto :fail
--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-echo 正在安装依赖...
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+echo 正在安装依赖（清华镜像）...
+python -m pip install --upgrade pip -i https://pypi.tuna.tsinghua.edu.cn/simple
+python -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 echo.
 echo 安装完成，运行 run.bat 启动网关。
 pause
